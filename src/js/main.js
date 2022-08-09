64ae9fe19d12e1da12727b5512ef84c189bc0ab5
Ivan <vasilev@engine-productions.de>
2022-08-09 21:53:31 +0200

    add image

---
+++ src/js/main.js
@@ -0,0 +1,5 @@
+// init Isotope
+var $grid = $('.grid').isotope({
+    itemSelector: '.element-item',
+    layoutMode: 'masonry',
+  });
--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,20 +1,26 @@
 import * as React from 'react'
 import { Link, graphql } from 'gatsby'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 import Layout from '../../components/layout'
 
 const BlogPage = ({ data }) => {
-
+  
   return (
     <Layout pageTitle="My Blog Posts">
       {
         data.allMdx.nodes.map(node => (
+       
           <article key={node.id}>
             <h2>
               <Link to={`/blog/${node.slug}`}>
                 {node.frontmatter.title}
               </Link>
             </h2>
-    
+            
+            <GatsbyImage
+              image= {getImage(node.frontmatter.hero_image)}
+              alt={node.frontmatter.hero_image_alt}
+            />
             
             <p>Posted: {node.frontmatter.date}</p>
           </article>
